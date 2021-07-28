037b42eefa0cd4a4f06b173f8dec5ccbb894d184
Diego Torres <43115342+Diego-TorresDev@users.noreply.github.com>
2021-07-27 23:33:13 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+include 'bd/BD.php';
+
+header('Access-Control-Allow-Origin: *');
+
+if($_SERVER['REQUEST_METHOD']=='GET'){
+    if(isset($_GET['id'])){
+        $query="select * from productos where id=".$_GET['id'];
+        $resultado=metodoGet($query);
+        echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
+    }else{
+        $query="select * from productos";
+        $resultado=metodoGet($query);
+        echo json_encode($resultado->fetchAll()); 
+    }
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='POST'){
+    unset($_POST['METHOD']);
+    $nombre=$_POST['nombre'];
+    $tipo=$_POST['tipo'];
+    $valor=$_POST['valor'];
+    $query="insert into productos(nombre, tipo, valor) values ('$nombre', '$tipo', '$valor')";
+    $queryAutoIncrement="select MAX(id) as id from productos";
+    $resultado=metodoPost($query, $queryAutoIncrement);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='PUT'){
+    unset($_POST['METHOD']);
+    $id=$_GET['id'];
+    $nombre=$_POST['nombre'];
+    $tipo=$_POST['tipo'];
+    $valor=$_POST['valor'];
+    $query="UPDATE productos SET nombre='$nombre', tipo='$tipo', valor='$valor' WHERE id='$id'";
+    $resultado=metodoPut($query);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='DELETE'){
+    unset($_POST['METHOD']);
+    $id=$_GET['id'];
+    $query="DELETE FROM productos WHERE id='$id'";
+    $resultado=metodoDelete($query);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+header("HTTP/1.1 400 Bad Request");
+
+
+?>
